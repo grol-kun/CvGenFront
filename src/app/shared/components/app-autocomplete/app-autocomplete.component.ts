8160f8f204108b2f28979d0756ef0e890233eb1f
Artem Pushnev <artem.pushnev@gmail.com>
2022-08-13 23:37:15 +0300

    [Refactor]: Refactor app-autocomplete

--- src/app/shared/components/app-autocomplete/app-autocomplete.component.ts
+++ src/app/shared/components/app-autocomplete/app-autocomplete.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input, Self } from '@angular/core';
-import { ControlValueAccessor, NgControl } from '@angular/forms';
+import { ControlValueAccessor, FormControl, NgControl } from '@angular/forms';
+import { Subject, takeUntil } from 'rxjs';
 
 interface Option {
   label: string;
   value: string;
   age: number;
 }
+//For example....
 
 @Component({
   selector: 'app-app-autocomplete',
@@ -13,11 +15,14 @@ interface Option {
   styleUrls: ['./app-autocomplete.component.scss']
 })
 export class AppAutocompleteComponent implements ControlValueAccessor {
-  @Input() public label?= 'Input';
-  @Input() public placeholder?= 'placeholder';
+  @Input() public label: string = 'Autocomplete';
+  @Input() public placeholder: string = 'placeholder';
+  private destroy$ = new Subject<void>();
+  public control = new FormControl();
   private onChange = (value: any) => { };
   private onTouched = () => { };
 
+  //For example,too....
   inputValue: Option = { label: 'Lucy', value: 'lucy', age: 20 };
   options: Option[] = [
     { label: 'Lucy', value: 'lucy', age: 20 },
@@ -28,6 +33,24 @@ export class AppAutocompleteComponent implements ControlValueAccessor {
     this.ngControl.valueAccessor = this;
   }
 
+  registerOnChange = (fn: (value: any) => {}) => this.onChange = fn;
+  registerOnTouched = (fn: () => {}) => this.onTouched = fn;
+
+  ngOnInit(): void {
+    this.control!.valueChanges
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((value) => {
+        this.onChange(value + '1');
+        console.log(`value ${value}`);
+        console.log(`control`, this.control);
+        console.log(`ngControl`, this.ngControl);
+      })
+  }
+
+  writeValue(value: any): void {
+    this.control.setValue(value, { emitEvent: false });
+  }
+
   compareFun = (o1: Option | string, o2: Option): boolean => {
     if (o1) {
       return typeof o1 === 'string' ? o1 === o2.label : o1.value === o2.value;
@@ -36,12 +59,8 @@ export class AppAutocompleteComponent implements ControlValueAccessor {
     }
   };
 
-  registerOnChange = (fn: (value: any) => {}) => this.onChange = fn;
-  registerOnTouched = (fn: () => {}) => this.onTouched = fn;
-
-  writeValue(value: any): void {
-    if (value) {
-      this.ngControl.control?.setValue(value, { emitEvent: false });
-    }
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
