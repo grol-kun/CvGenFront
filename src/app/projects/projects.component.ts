884fe01af66f3ff57052afd2a590b29f0b049647
Artem Pushnev <artem.pushnev@gmail.com>
2022-08-14 14:28:12 +0300

    Merge with feature/app-autocomplete

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -24,7 +24,7 @@ export class ProjectsComponent implements OnInit {
       password: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(10)]],
       anoter: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(10)]],
       data: [null, [Validators.required]],
-      auto: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(10)]]
+      auto: ['', [Validators.required]]
     });
     this.logForm();
   }
