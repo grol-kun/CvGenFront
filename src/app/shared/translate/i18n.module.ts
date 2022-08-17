cefc00f306471c8c994bc370abb00c17ccae568a
neverniu <neverniu@aol.com>
2022-08-17 13:13:05 +0300

    [fix]: remove insufficient providers, imports

--- src/app/shared/translate/i18n.module.ts
+++ src/app/shared/translate/i18n.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { I18nComponent } from './i18n/i18n.component';
 import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
-import { TranslateLoader, TranslateModule, TranslateService, TranslateStore } from '@ngx-translate/core';
+import { TranslateLoader, TranslateModule, TranslateStore } from '@ngx-translate/core';
 import { HttpClient } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
@@ -12,26 +12,8 @@ export function HttpLoaderFactory(http: HttpClient): TranslateLoader {
 
 @NgModule({
   declarations: [I18nComponent],
-  imports: [
-    CommonModule,
-    NzDropDownModule,
-    TranslateModule.forChild({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient],
-      },
-      isolate: false,
-      extend: true,
-    }),
-  ],
+  imports: [CommonModule, NzDropDownModule],
   providers: [TranslateStore],
   exports: [I18nComponent, TranslateModule],
 })
-export class I18nModule {
-  constructor(private translate: TranslateService) {
-    translate.addLangs(['en', 'ru']);
-    translate.setDefaultLang('en');
-    translate.use('en');
-  }
-}
+export class I18nModule {}
