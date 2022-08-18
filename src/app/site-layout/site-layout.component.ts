d49c0c10c9c223f5da94617075c5d369024992ec
Artem Pushnev <artem.pushnev@gmail.com>
2022-08-18 17:59:50 +0300

    [refactor]: Refactor change detection strategy

--- src/app/site-layout/site-layout.component.ts
+++ src/app/site-layout/site-layout.component.ts
@@ -1,18 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { BehaviorSubject, Observable, tap } from 'rxjs';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+} from '@angular/core';
+import { Observable } from 'rxjs';
 import { LoaderService } from '../shared/services/loader.service';
 
 @Component({
   selector: 'app-site-layout',
   templateUrl: './site-layout.component.html',
   styleUrls: ['./site-layout.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SiteLayoutComponent implements OnInit {
   loader$?: Observable<boolean>;
 
-  constructor(private loaderService: LoaderService) {}
+  constructor(
+    private loaderService: LoaderService,
+    private cd: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
     this.loader$ = this.loaderService.loaderStatus$;
+  }
+
+  ngAfterViewInit() {
+    this.cd.detectChanges();
   }
 }
