d81f2c2ce06be28c4091e7cb4b035097b7ebc837
neverniu <neverniu@aol.com>
2022-08-24 16:11:04 +0300

    [refactor]: add constants, move svg

---
+++ src/app/shared/models/constants/styles.ts
@@ -0,0 +1,59 @@
+export const STYLES = {
+  list: {
+    margin: [0, 0, 0, 16],
+    fontSize: 10,
+  },
+  headerMain: {
+    margin: [0, 0, 0, 24],
+    color: '#1F2937',
+  },
+  headerSide: {
+    fontSize: 10,
+    color: 'white',
+    margin: [0, 0, 0, 24],
+  },
+  sectionTitle: {
+    fontSize: 16,
+    lineHeight: 1.1,
+    bold: true,
+    margin: [0, 0, 0, 16],
+  },
+  name: {
+    color: 'white',
+    fontSize: 18,
+    margin: [0, 0, 0, 4],
+    bold: true,
+  },
+  title: {
+    color: 'white',
+    fontSize: 14,
+    margin: [0, 0, 0, 4],
+    bold: true,
+  },
+  headerLinks: {
+    margin: [0, 0, 0, 6],
+  },
+  listItem: {
+    fontSize: 9,
+    margin: [0, 0, 0, 8],
+  },
+  listItemHeader: {
+    color: '#C63031',
+    fontSize: 10,
+    bold: true,
+    margin: [0, 0, 0, 3],
+  },
+  listItemSubHeader: {
+    fontSize: 8,
+    margin: [0, 0, 0, 3],
+  },
+  listItemDesc: {
+    lineHeight: 1.1,
+    fontSize: 9,
+  },
+  headerListItem: {
+    color: 'white',
+    lineHeight: 1.2,
+    margin: [0, 0, 0, 3],
+  },
+};
