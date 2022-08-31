0f3afc52023d57fdf30e049ab24dabb47e1359f1
Artem Pushnev <artem.pushnev@gmail.com>
2022-08-31 17:48:13 +0300

    [refactor]: Refactor filter.pipe

--- src/app/shared/pipes/filter.pipe.ts
+++ src/app/shared/pipes/filter.pipe.ts
@@ -1,14 +1,25 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'attributesNameFilter',
+  name: 'filter',
 })
 export class FilterPipe implements PipeTransform {
-  transform(data: any[], searchText: string) {
+  transform(data: any[], searchText: string, params: string[] | string) {
     if (data.length === 0 || searchText === '') {
       return data;
     }
 
-    return data.filter((item) => item.attributes.name.toLowerCase().indexOf(searchText.toLowerCase()) !== -1);
+    if (typeof params === 'string') {
+      return data.filter((item) => item[params as string].toLowerCase().indexOf(searchText.toLowerCase()) !== -1);
+    }
+
+    for (let i = 0; i < params.length; i++) {}
+
+    return data.filter((item) => {
+      for (let i = 0; i < params.length; i++) {
+        item = item[params[i]];
+      }
+      return item.toLowerCase().indexOf(searchText.toLowerCase()) !== -1;
+    });
   }
 }
