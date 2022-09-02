a435cd4b00c82dec9081c439b7107ab66e7393ee
neverniu <neverniu@aol.com>
2022-09-02 21:57:25 +0300

    [fix]: remove useless import

--- src/app/shared/services/ability.service.ts
+++ src/app/shared/services/ability.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Ability } from '../models/interfaces/ability';
 import { ResponseOneEntity } from '../models/interfaces/response-one-entity';
 
 @Injectable({
