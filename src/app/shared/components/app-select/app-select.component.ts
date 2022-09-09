70a685759087661ab3756623319fe7d74d58cfde
Artem Pushnev <artem.pushnev@gmail.com>
2022-09-09 15:34:28 +0300

    [9]: Add markAsTouched

--- src/app/shared/components/app-select/app-select.component.ts
+++ src/app/shared/components/app-select/app-select.component.ts
@@ -1,14 +1,14 @@
-import { Component, Input, OnDestroy, OnInit, Self } from '@angular/core';
+import { Component, Input, OnDestroy, Self } from '@angular/core';
 import { ControlValueAccessor, FormBuilder, FormControl, NgControl } from '@angular/forms';
 import { NzSelectModeType } from 'ng-zorro-antd/select';
-import { Subject, takeUntil } from 'rxjs';
+import { Subject, takeUntil, tap } from 'rxjs';
 
 @Component({
   selector: 'app-select',
   templateUrl: './app-select.component.html',
   styleUrls: ['./app-select.component.scss'],
 })
-export class AppSelectComponent implements ControlValueAccessor, OnInit, OnDestroy {
+export class AppSelectComponent implements ControlValueAccessor, OnDestroy {
   @Input() label = 'Input';
   @Input() options: string[] = [];
   @Input() placeholder: string = 'Please select...';
@@ -16,18 +16,21 @@ export class AppSelectComponent implements ControlValueAccessor, OnInit, OnDestr
 
   control = new FormControl();
   private destroy$ = new Subject<void>();
-  private onChange = (value: any) => {};
   private onTouched = () => {};
 
   constructor(@Self() public ngControl: NgControl, private fb: FormBuilder) {
     this.ngControl.valueAccessor = this;
   }
 
-  registerOnChange = (fn: (value: any) => {}) => (this.onChange = fn);
   registerOnTouched = (fn: () => {}) => (this.onTouched = fn);
 
-  ngOnInit(): void {
-    this.control!.valueChanges.pipe(takeUntil(this.destroy$)).subscribe((value) => this.onChange(value));
+  public registerOnChange(fn: any): void {
+    this.control.valueChanges
+      .pipe(
+        tap(() => this.control.markAsTouched()),
+        takeUntil(this.destroy$)
+      )
+      .subscribe(fn);
   }
 
   writeValue(value: any): void {
