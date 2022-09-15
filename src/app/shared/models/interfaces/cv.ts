2032802d4b26c641df851d0ec50c0c4c10704128
Artsiom <artsiom.pushniou@innowise-group.com>
2022-09-15 23:03:27 +0300

    [3]: Add separation of User info to each Cv

--- src/app/shared/models/interfaces/cv.ts
+++ src/app/shared/models/interfaces/cv.ts
@@ -1,17 +1,5 @@
-import { DataProject } from './data-project';
-import { Language } from './language';
-import { Skill } from './skill';
-
+import { CvAttributes } from './cv-attributes';
 export interface Cv {
   id: number;
-  attributes: {
-    name: string;
-    description: string;
-    projects: DataProject | null;
-    languages: Language[];
-    skills: Skill[];
-    createdAt: string;
-    updatedAt: string;
-    publishedAt: string;
-  };
+  attributes: CvAttributes;
 }
